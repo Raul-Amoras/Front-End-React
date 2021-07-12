30ea42ea58ab6ecfe71f3ca56ce83700dbaa18d3
Raul Amoras <raul.amoras@gmail.com>
2021-07-12 20:52:44 -0300

    adicionado a funcao async de cadastro

--- src/App.js
+++ src/App.js
@@ -14,9 +14,31 @@ export default function App() {
     })
   }, [])
 
-  function novoProjeto() {
-    //projects.push(`projet ${Date.now()}`)
-    setProjects([...projects, `Project ${Date.now()}`])
+  async function novoProjeto() {
+    const response = await api.post('projects', {
+      nome: 'amoras',
+      nomeFantasia: 'Raul amoras',
+      tipoPessoa: 'F',
+      tipoCliente: 'C',
+      cnpj_Cpf: '790.630.210-97',
+      rg: '7333078',
+      InscricaoEstadual: 'Null',
+      telefone: '(91)98485-7415',
+      endereco: 'Rua vitoria',
+      bairro: 'Nova esperanca',
+      cep: '68625000',
+      codMunicipio: '001',
+      numero: '325',
+      complemento: 'Null',
+      email: 'raul.amoras@gmail.com',
+      dataNascimento: '11/09/1996',
+      sexo: 'M',
+      dataCadastro: '01/01/2020',
+      ativo: 'S'
+    })
+    const project = response.data
+
+    setProjects([...projects, project])
   }
 
   return (
