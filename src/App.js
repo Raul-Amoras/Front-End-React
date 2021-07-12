048e76fd3ed6d11ea9487a3924e6fbe6a11777d2
Raul Amoras <raul.amoras@gmail.com>
2021-07-11 23:58:11 -0300

    criado pasta de estilizaçao e repositorio de imagem

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 
-export default function App(){
-  const [projects, setProjects] = useState( ['HTML', 'CSS'])
+import backgroudImg from './assets/backgroud.jfif'
 
-  function novoProjeto (){
+import './styles/style.css'
+
+export default function App() {
+  const [projects, setProjects] = useState(['HTML', 'CSS'])
+
+  function novoProjeto() {
     //projects.push(`projet ${Date.now()}`)
     setProjects([...projects, `Project ${Date.now()}`])
     console.log(projects)
   }
 
-
   return (
-    <> 
-    <Header title="Projects">
-      <ul>
-        {projects.map(project =>(
-          <li key={project}>{project}</li>
-        ))}
-      </ul>
-
-      <button onClick={novoProjeto}>Novo Projeto</button>
-    </Header>
+    <>
+      <Header title="Projects">
+        <img className="img" src={backgroudImg} alt="imagem de um lugar" />
+        <ul>
+          {projects.map(project => (
+            <li key={project}>{project}</li>
+          ))}
+        </ul>
+        <button onClick={novoProjeto}>Novo Projeto</button>
+      </Header>
     </>
-      
   )
 }
