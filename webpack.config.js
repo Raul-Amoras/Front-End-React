050a4f57e95c67bd6c2766781db512bc191f85c7
Raul Amoras <raul.amoras@gmail.com>
2021-07-11 23:56:40 -0300

    configurado para usar imagem

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
           {loader: 'style-loader'},
           {loader: 'css-loader'},
         ]
+      },
+      {
+        test:/.*\.(git|png|jpe?g|jfif)/,
+        use:{
+          loader: 'file-loader',
+        }
       }
     ]
   }
